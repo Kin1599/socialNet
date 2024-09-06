b38a04a1b22ee8d22bd8b721b2da9978d038d93b
Kin1599 <dkonopliannikov05@mail.ru>
2024-09-06 19:39:20 +0300

    init

---
+++ frontend/src/app/App.js
@@ -0,0 +1,12 @@
+import LoginPage from '../pages/LoginPage/LoginPage';
+import '../shared/styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage/>
+    </div>
+  );
+}
+
+export default App;
