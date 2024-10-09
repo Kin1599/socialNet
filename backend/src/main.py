a6f7e73e3e1c073cddb0593cb36ea1ddda2bb6d5
Kin1599 <dkonopliannikov05@mail.ru>
2024-10-09 16:47:16 +0300

    test

--- backend/src/main.py
+++ backend/src/main.py
@@ -15,4 +15,4 @@ app = FastAPI(
 app.include_router(auth_router, prefix='/auth')
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8081, reload=True)
+    uvicorn.run("main:app", reload=True)
---
+++ frontend/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import cl from './MainPage.module.scss'
+
+function MainPage() {
+  return (
+    <div className={cl.mainPage}>
+        MainPage
+    </div>
+  )
+}
+
+export default MainPage
