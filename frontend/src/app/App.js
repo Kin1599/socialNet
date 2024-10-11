65924cde4777a3c66daeee27d9840688a6a75d2b
publicprofile123 <ilya.grinenko@gmail.com>
2024-10-11 14:05:49 +0300

    Revert "Merge pull request #1 from Kin1599/ilya"
    This reverts commit bd0586d936315b999cfaf531f78b343a3156df7a, reversing
    changes made to 8efd41f458d581a523043e0fc74d8e1690f95308.

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,16 +1,10 @@
-import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import '../shared/styles/App.scss';
-import MainPage from '../pages/MainPage/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='*' element={<Navigate to="/" replace/>}/>
-        <Route path="/login" element={<LoginPage/>}/>
-        <Route path="/" element={<MainPage/>}/>
-      </Routes>
+      <LoginPage/>
     </div>
   );
 }
