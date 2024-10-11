dc262e72010822d16704e7e73c737f29d1639ab5
publicprofile123 <ilya.grinenko@gmail.com>
2024-10-11 22:46:53 +0300

    rabvers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,15 @@
-from fastapi import FastAPI, Body, Depends
-import routers.auth_router
-import routers.user_page_router
+from fastapi import FastAPI, Depends
 import uvicorn
+
+from app.routers import auth_router, user_page_router
 from app.auth.auth_bearer import JWTBearer
 from app.db_models import Base
 from app.config.database import engine
 
 app = FastAPI()
 
-app.include_router(routers.auth_router.router)
-app.include_router(routers.user_page_router.router)
+app.include_router(auth_router.router)
+app.include_router(user_page_router.router)
 
 @app.get("/", dependencies=[Depends(JWTBearer())], tags=[""])
 def home():
